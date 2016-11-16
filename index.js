a4b8128d9e6cfa4071e86bb55d51b2375abe708a
Alexander <alexaa2007@gmail.com>
2016-11-16 16:32:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,28 @@ var app = express();
 app.use(cors());
 
 app.get('/', function (req, res) {
-  const un = req.query.username;
   let result = '';
 
-  if (un.length === 0) {
+  const un = req.query.username;
+
+  if (typeof un === 'undefined' || un.length === 0) {
+    console.log('We got undefined username');
     return res.send('Invalid username');
   }
 
   console.log('We get:' + un);
   let re = new RegExp();
-  re = /((https?:?)?(\/\/)?(\w+\.?){2,}?(\/))?(\w+)/;
 
-  result = '@' + un.match(re)[6];
+  re = /((https?:?)?(\/\/)?([A-z0-9\-]+\.?){2,}?(\/))?(@?\w+(\.\w+)?)/;
+  result = un.match(re)[6];
+
+  if (result[0] !== '@') {
+    result = '@' + result;
+  }
+
   console.log('Result:' + result);
   res.send(String(result));
+  console.log('------');
 });
 
 app.listen(3001, function() {
